bf2c2160fe2654df18078756a8ea6103713113ad
Salman Ahmed <dev.salmanahmed47@gmail.com>
2024-10-23 21:44:44 +0500

    Krdia

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,24 @@
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
 
-void main() async {
-  WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp();
+// void main() async {
+//   WidgetsFlutterBinding.ensureInitialized();
+//   await Firebase.initializeApp();
+//   runApp(const MyApp());
+// }
+Future<void> main() async {
+  WidgetsBinding widgetsBinding = WidgetsFlutterBinding.ensureInitialized();
+
+  try {
+    await Firebase.initializeApp(
+      options: DefaultFirebaseOptions.currentPlatform,
+    );
+    // print("******* $firebase *******");
+  } catch (e) {
+    // handle erro
+    // print('error');
+  }
   runApp(const MyApp());
 }
 
