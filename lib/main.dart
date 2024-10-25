5ffce07915493caac24696c87b4531abefe50296
Salman Ahmed <dev.salmanahmed47@gmail.com>
2024-10-25 16:55:19 +0500

    AI code implementation

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
-import 'package:interestin_app/pages/home.dart';
 import 'firebase_options.dart';
 
 Future<void> main() async {
